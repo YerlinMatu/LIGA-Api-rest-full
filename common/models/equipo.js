ae6da76bd0a31f9f42b9185576203b0e77a50731
Yerlin Matu <yerlinmatu@MacBook-Pro-de-Yerlin.local>
2018-11-25 19:28:29 -0500

    Added Hook for to add jugadores in equipos

--- common/models/equipo.js
+++ common/models/equipo.js
@@ -1,5 +1,30 @@
 'use strict';
 
 module.exports = function(Equipo) {
+  Equipo.jugadores = function (idEquipo, idJugador, cb) {
+    Equipo.findById(idEquipo, (err, equipo) => {
+      equipo.jugadores.push(idJugador);
+      equipo.updateAttribute('jugadores', equipo.jugadores, (err, equipo) => {
+        cb(null, equipo);
+      });
+    })
+  }
 
+  Equipo.remoteMethod('jugadores', {
+    description: 'Agregar jugadores al equipo',
+    accepts: [
+      {
+        arg: 'idEquipo',
+        type: 'string',
+      },
+      {
+        arg: 'idJugador',
+        type: 'string',
+      }
+    ],
+    returns: {
+      arg: 'equipo',
+      type: 'object',  
+    }
+  }) 
 };
